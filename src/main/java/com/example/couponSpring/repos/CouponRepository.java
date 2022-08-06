c2fb6d78f87a3f9fa154a743b6b2037f159f6319
tomer <tomerab00@gmail.com>
2022-08-06 11:45:18 +0300

    fixed native query shema

--- src/main/java/com/example/couponSpring/repos/CouponRepository.java
+++ src/main/java/com/example/couponSpring/repos/CouponRepository.java
@@ -26,14 +26,14 @@ public interface CouponRepository extends JpaRepository<Coupon, Integer> {
 
     boolean existsByCompany(Company company);
 
-    @Query(value = "SELECT * FROM `spring-147`.coupons inner join `spring-147`.customers_vs_coupons on `spring-147`.coupons.id = `spring-147`.customers_vs_coupons.coupon_id where `spring-147`.customers_vs_coupons.customer_id = ?",
+    @Query(value = "SELECT * FROM coupons inner join customers_vs_coupons on `spring-147`.coupons.id = customers_vs_coupons.coupon_id where customers_vs_coupons.customer_id = ?",
             nativeQuery = true)
     List<Coupon> findCustomerCoupons(int customerId);
 
-    @Query(value = "SELECT * FROM `spring-147`.coupons inner join `spring-147`.customers_vs_coupons on `spring-147`.coupons.id = `spring-147`.customers_vs_coupons.coupon_id where `spring-147`.customers_vs_coupons.customer_id = ? and `spring-147`.coupons.category = ?",
+    @Query(value = "SELECT * FROM coupons inner join `spring-147`.customers_vs_coupons on coupons.id = customers_vs_coupons.coupon_id where customers_vs_coupons.customer_id = ? and coupons.category = ?",
             nativeQuery = true)
     List<Coupon> findCustomerCouponsByCategory(int customerId, int category);
-    @Query(value = "SELECT * FROM `spring-147`.coupons inner join `spring-147`.customers_vs_coupons on `spring-147`.coupons.id = `spring-147`.customers_vs_coupons.coupon_id where `spring-147`.customers_vs_coupons.customer_id = ? and `spring-147`.coupons.price >= ?",
+    @Query(value = "SELECT * FROM coupons inner join customers_vs_coupons on coupons.id = customers_vs_coupons.coupon_id where customers_vs_coupons.customer_id = ? and coupons.price >= ?",
             nativeQuery = true)
     List<Coupon> findCustomerCouponsByMaxPrice(int customerId, double price);
 
--- src/main/java/com/example/couponSpring/repos/CustomerRepository.java
+++ src/main/java/com/example/couponSpring/repos/CustomerRepository.java
@@ -13,7 +13,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     boolean existsByEmail(String email);
     boolean existsById(int id);
     boolean existsByEmailAndPassword(String email, String password);
-    @Query(value = "SELECT CASE WHEN exists (SELECT * FROM `spring-147`.`customers_vs_coupons` where customer_id = ? and coupon_id = ?) then 'true' else 'false' end",
+    @Query(value = "SELECT CASE WHEN exists (SELECT * FROM `customers_vs_coupons` where customer_id = ? and coupon_id = ?) then 'true' else 'false' end",
             nativeQuery = true)
     boolean existsByCustomerAndCoupon(int customerId, int couponId);
     Optional<Customer> findByEmail(String email);
